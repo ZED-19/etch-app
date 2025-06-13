807ebe4f71c8dae61fdcd9a8e5169e2595a42f1d
ZED-19 <zinoumb19@gmail.com>
2025-06-13 12:59:38 +0100

    Add initial configuration for Tailwind CSS and NativeWind integration
    - Created babel.config.js for Babel presets.
    - Added global.css for Tailwind CSS directives.
    - Configured metro.config.js to use NativeWind with global.css.
    - Introduced type definitions for NativeWind in nativewind-env.d.ts and nativewind.d.ts.
    - Updated package.json and package-lock.json to include NativeWind and Tailwind CSS dependencies.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Updated tsconfig.json to include NativeWind type definitions.
    - Modified layout files to import global styles and added a new profile tab with MaterialIcons.
    - Created profile.tsx for the new profile screen.
    - Adjusted IconSymbol component imports for consistency.

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView className='flex-1 justify-center items-center'>
+            <Text className='text-white'>Profile</Text>
+        </SafeAreaView>
+    );
+}
